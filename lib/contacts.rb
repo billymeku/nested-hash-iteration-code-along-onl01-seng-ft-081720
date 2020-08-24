c4093b62032b75104b5519a4bb6481ba74bdf0aa
Blen Wondimagegn <mekuriablen@gmail.com>
2020-08-24 09:31:50 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -19,7 +19,11 @@ def remove_strawberry(contacts)
      data.each do |attribute, value|
        
     if attribute == :favorite_ice_cream_flavors
-    
+       value.each do |flavor|
+         if flavor == "strawberry"
+           flavor.delet
+      
+     end
    end 
   end 
 end
